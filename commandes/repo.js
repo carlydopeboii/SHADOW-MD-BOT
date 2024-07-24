cf8a29ac5a579a2ea591f663453245500eeaeeb7
carlydopeboii <164931149+carlydopeboii@users.noreply.github.com>
2024-07-23 23:13:09 -0400

    𝐒𝐇𝐀𝐃𝐎𝐖 𝐌𝐀𝐊𝐄 𝐀 𝐑𝐄𝐌𝐀𝐒𝐓𝐄𝐃 ʕʘ̅͜ʘ̅ʔ

---
+++ commandes/repo.js
@@ -0,0 +1,43 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { zokou } = require("../framework/zokou");
+
+zokou({ nomCom: "repo", catégorie:"Général", reaction: "🌏", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+  const githubRepo = 'https://api.github.com/repos/carlydopeboii/SHADOW-MD-BOT';
+  const img = 'https://telegra.ph/file/c9c0af94510158016f7c6.jpg';
+
+  try {
+    const response = await fetch(githubRepo);
+    const data = await response.json();
+
+    if (data) {
+      const repoInfo = {
+        stars: data.stargazers_count,
+        forks: data.forks_count,
+        lastUpdate: data.updated_at,
+        owner: data.owner.login,
+      };
+
+      const releaseDate = new Date(data.created_at).toLocaleDateString('en-GB');
+      const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
+
+      const gitdata = `salut 👋 
+voici *𝐒𝐡𝐚𝐝𝐨𝐰-𝐌𝐝.*\n TU DEPLOYER A PARTIR DU *REPO*, *SESSION_ID*  ' https://habot.000webhostapp.com/ '
+
+🗼 *REPOSITORY:* ${data.html_url}
+✨ *STARS:* ${repoInfo.stars}
+🧧 *FORKS:* ${repoInfo.forks}
+📅 *RELEASE DATE:* ${releaseDate}
+🕐 *UPDATE ON:* ${repoInfo.lastUpdate}
+👨‍💻 *OWNER:* *𝐒𝐡𝐚𝐝𝐨𝐰-𝐖𝐫𝐥𝐝*
+__________________________________
+            *Made With* 𝐒𝐡𝐚𝐝𝐨𝐰-𝐖𝐫𝐥𝐝`;
+
+      await zk.sendMessage(dest, { image: { url: img }, caption: gitdata });
+    } else {
+      console.log("Could not fetch data");
+    }
+  } catch (error) {
+    console.log("Error fetching data:", error);
+  }
+});
