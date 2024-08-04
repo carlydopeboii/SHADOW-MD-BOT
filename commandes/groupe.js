91148d7bb110ddbbb7d8450b3af2ad5b764c419a
𝐒𝐡𝐚𝐝𝐨𝐰-𝐖𝐫𝐥𝐝 <164931149+carlydopeboii@users.noreply.github.com>
2024-08-04 09:34:11 -0400

    𝐒𝐇𝐀𝐃𝐎𝐖 𝐌𝐀𝐊𝐄 𝐀 𝐑𝐄𝐌𝐀𝐒𝐓𝐄𝐃 ʕʘ̅͜ʘ̅ʔ

--- commandes/groupe.js
+++ commandes/groupe.js
@@ -27,7 +27,7 @@ zokou({ nomCom: "appel", categorie: "Groupe", reaction: "📣" }, async (dest, z
   let membresGroupe = verifGroupe ? await infosGroupe.participants : ""
   var tag = ""; 
   tag += `✦━━━━━━━❂❂━━━━━━━✦\n  
-              ❄ *SHADOW-MD* ❄
+              ❄ *𝐒𝐇𝐀𝐃𝐎𝐖 𝐌𝐃* ❄
 ╔════❖═════❖════╗\n
 👥 Groupe : ${nomGroupe} 🚀 
 👤 Auteur : *${nomAuteurMessage}* 👋 
