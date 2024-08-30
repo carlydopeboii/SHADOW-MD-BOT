d75afaa49bb4b04a65bad934cdbadc3e2e39a5d2
𝐒𝐡𝐚𝐝𝐨𝐰-𝐖𝐫𝐥𝐝 <164931149+carlydopeboii@users.noreply.github.com>
2024-08-29 21:28:16 -0400

    𝐒𝐇𝐀𝐃𝐎𝐖 𝐌𝐀𝐊𝐄 𝐀 𝐑𝐄𝐌𝐀𝐒𝐓𝐄𝐃 ʕʘ̅͜ʘ̅ʔ

---
+++ commandes/scan.js
@@ -0,0 +1,171 @@
+
+const util = require('util');
+const fs = require('fs-extra');
+const { adams } = require(__dirname + "/../Ibrahim/adams");
+const { format } = require(__dirname + "/../Ibrahim/mesfonctions");
+const os = require("os");
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../config");
+const more = String.fromCharCode(8206)
+const readmore = more.repeat(4001)
+
+adams({ nomCom: "scan", categorie: "Général" }, async (dest, zk, commandeOptions) => {
+    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
+    let { cm } = require(__dirname + "/../Ibrahim//adams");
+    var coms = {};
+    var mode = "public";
+    
+    if ((s.MODE).toLocaleLowerCase() != "oui") {
+        mode = "privé";
+    }
+
+
+    
+
+    cm.map(async (com, index) => {
+        if (!coms[com.categorie])
+            coms[com.categorie] = [];
+        coms[com.categorie].push(com.nomCom);
+    });
+
+    moment.tz.setDefault('Etc/GMT');
+
+// Créer une date et une heure en GMT
+const temps = moment().format('HH:mm:ss');
+const date = moment().format('DD/MM/YYYY');
+
+  let infoMsg =  `
+ *Tap on the link to get session*
+  
+ https://shadow-session-id-generator.onrender.com
+ 
+ 
+ *STEPS TO GET SESSION*
+ 
+ 1. Ouvrir le lien
+ 2. Entrez votre numéro WhatsApp avec votre code pays, par exemple : 254,255. Et appuyez sur Soumettre
+ 3. Shadow Wrld vous enverra un code. Copiez ce code. Ensuite, WhatsApp enverra une notification
+ 4. Appuyez sur cette notification puis entrez le code que Shadow Wrld vous a envoyé
+ 5. Il se chargera pendant un certain temps, puis Shadow Wrld enverra une longue session dans votre boîte de réception sur WhatsApp à votre propre numéro
+ 6. Copiez cette longue session et envoyez-la à votre déployeur.
+ 
+ 🚘🚘🚘
+ `;
+    
+let menuMsg = `
+> Made by : ©𝐒𝐡𝐚𝐝𝐨𝐰-𝐖𝐫𝐥𝐝 
+`;
+
+   var lien = mybotpic();
+
+   if (lien.match(/\.(mp4|gif)$/i)) {
+    try {
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *adams*, déveloper adams Tech" , gifPlayback : true }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+// Vérification pour .jpeg ou .png
+else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+    try {
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *adams*, déveloper adams Tech" }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+else {
+    
+    repondre(infoMsg + menuMsg);
+    
+}
+
+}); 
+
+
+/*const util = require('util');
+const fs = require('fs-extra');
+const { zokou } = require(__dirname + "/../framework/zokou");
+const { format } = require(__dirname + "/../framework/mesfonctions");
+const os = require("os");
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../set");
+const more = String.fromCharCode(8206)
+const readmore = more.repeat(4001)
+
+zokou({ nomCom: "script", categorie: "General" }, async (dest, zk, commandeOptions) => {
+    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
+    let { cm } = require(__dirname + "/../framework//zokou");
+    var coms = {};
+    var mode = "public";
+    
+    if ((s.MODE).toLocaleLowerCase() != "yes") {
+        mode = "private";
+    }
+
+
+    
+
+    cm.map(async (com, index) => {
+        if (!coms[com.categorie])
+            coms[com.categorie] = [];
+        coms[com.categorie].push(com.nomCom);
+    });
+
+    moment.tz.setDefault('Etc/GMT');
+
+// Créer une date et une heure en GMT
+const temps = moment().format('HH:mm:ss');
+const date = moment().format('DD/MM/YYYY');
+
+  let infoMsg =  `
+   *SHADOW MD IMPORTANT INFO* 
+❒───────────────────❒
+*GITHUB LINK*
+> https://github.com/carlydopeboii/SHADOW-MD-BOT
+
+
+*FOR MORE INFO TAP ON THE LINK BELOW*
+> https://github.com/carlydopeboii
+╭───────────────────❒
+│❒⁠⁠⁠⁠ *RAM* : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
+│❒⁠⁠⁠⁠ *DEV* : *Shadow Wrld*
+⁠⁠⁠⁠╰───────────────────❒
+  `;
+    
+let menuMsg = `
+     𝑆𝐻𝐴𝐷𝑂𝑊 𝑊𝑅𝐿𝐷 𝑆𝐶𝐼𝐸𝑁𝐶𝐸
+
+❒────────────────────❒`;
+
+   var lien = mybotpic();
+
+   if (lien.match(/\.(mp4|gif)$/i)) {
+    try {
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Beltahmd*, déveloper Beltah Tech" , gifPlayback : true }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+// Vérification pour .jpeg ou .png
+else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+    try {
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Beltahmd*, déveloper Beltah Tech" }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+else {
+    
+    repondre(infoMsg + menuMsg);
+    
+}
+
+});*/
